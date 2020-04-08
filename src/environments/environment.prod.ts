3a0737f1ffd9babeb33d39c25b34c5acf1a0e431
actionanand <meetanandns@gmail.com>
2020-04-09 00:04:19 +0530

    get api dta

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  url: 'https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats',
-  host: 'covid-19-coronavirus-statistics.p.rapidapi.com',
-  key: '59096b09e6msha7c22a8c806d6b6p1f1bf3jsn46ba13ae97bb'
-};
